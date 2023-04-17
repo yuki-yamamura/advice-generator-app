983dceabeee5d81c6493d11ad19a252b6b668a0d
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-04-18 02:11:16 +0900

    chore: add font and colors to Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+      colors: {
+        lightCyan: 'hsl(193, 38%, 86%)',
+        neonGreen: 'hsl(150, 100%, 66%)',
+        grayishBlue: 'hsl(217, 19%, 38%)',
+        darkGrayishBlue: 'hsl(217, 19%, 24%)',
+        darkBlue: 'hsl(218, 23%, 16%)',
+      },
+      screens: {
+        xs: '540px',
+      },
+    },
   },
   plugins: [],
 };
